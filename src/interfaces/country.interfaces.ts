868dae3d095090b8aa26ca75dabb4d0f559b9f1a
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-10-05 14:33:03 -0300

    Feat: created interfaces and request to get population data

--- src/interfaces/country.interfaces.ts
+++ src/interfaces/country.interfaces.ts
@@ -19,14 +19,32 @@ export interface ICountry {
   borders: IBorder[];
 }
 
-export interface IFlag{
-    name:string,
-    flag:string,
-    iso2:string,
-    iso3:string
+export interface IFlag {
+  name: string;
+  flag: string;
+  iso2: string;
+  iso3: string;
 }
-export interface IFlagResponse{
-    error:boolean,
-    msg:string,
-    data:IFlag[]
+export interface IFlagResponse {
+  error: boolean;
+  msg: string;
+  data: IFlag[];
+}
+
+interface PopulationCount {
+  year: number;
+  value: number;
+}
+
+export interface CountryData {
+  country: string;
+  code: string;
+  iso3: string;
+  populationCounts: PopulationCount[];
+}
+
+export interface PopulationResponse {
+  error: boolean;
+  msg: string;
+  data: CountryData[];
 }
--- src/services/country.service.ts
+++ src/services/country.service.ts
@@ -1,9 +1,9 @@
 import { AxiosInterceptor } from "../interceptor/axios.interceptor";
-import { ICountry, ICountryPreview, IFlag, IFlagResponse } from '@/interfaces/country.interfaces';
+import { ICountry, ICountryPreview, IFlag, IFlagResponse, PopulationResponse } from '@/interfaces/country.interfaces';
 
 const axios = new AxiosInterceptor().getAxiosInstance();
 const api = 'https://date.nager.at/api/v3';
-const flagsApi ='https://countriesnow.space/api/v0.1/countries/flag/images'
+const api2 ='https://countriesnow.space/api/v0.1/countries'
 
 export const fetchCountries = async (): Promise<ICountryPreview[]> => {
   try {
@@ -27,7 +27,17 @@ export const fetchCountryByCountryCode = async (countryCode:string): Promise<ICo
 
 export const fetchCountriesFlags = async (): Promise<IFlagResponse> => {
   try {
-    const response = await axios.get(`${flagsApi}`);
+    const response = await axios.get(`${api2}/flag/images`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching countries:', error);
+    throw new Error('Failed to fetch countries');
+  }
+};
+
+export const fetchCountriesPopulation = async (): Promise<PopulationResponse> => {
+  try {
+    const response = await axios.get(`${api2}/population`);
     return response.data;
   } catch (error) {
     console.error('Error fetching countries:', error);
